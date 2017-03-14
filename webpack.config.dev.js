bd9e7566cbe3db5f2541e8ebfe7b668ffd3b4620
= <=>
2017-03-14 13:11:25 +0800

    configured react with node

---
+++ webpack.config.dev.js
@@ -0,0 +1,25 @@
+import path from 'path'
+import webpack from 'webpack';
+
+export default {
+  entry: path.join(__dirname, '/src/index.js'),
+  output: {
+    path: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [
+          path.join(__dirname, 'src'),
+        ],
+        loaders: [ 'babel' ]
+      },
+       { test: /\.css$/, loader: "style-loader!css-loader" },
+    ]
+  },
+  resolve: {
+    extentions: [ '', '.js' ]
+  }
+}
